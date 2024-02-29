b309874f88f50b0ff9a3e4ab8514982aa576e552
Mahala Wise <mamawise@iu.edu.com>
2024-02-29 18:08:04 -0500

    "fixed issues"

--- ciphers_project/ciphers_project/settings.py
+++ ciphers_project/ciphers_project/settings.py
@@ -128,5 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "https://mahalawise.github.io",
-    "https:/localhost:3000"
+    "https://localhost:3000"
 ]
